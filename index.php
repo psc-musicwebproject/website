b104954731440b9dc7c0f37f31560fe9e89a88d8
Cartoon Kritthapth Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2025-08-21 12:11:12 +0700

    update: login page to use id instead of username

--- index.php
+++ index.php
@@ -1,26 +1,9 @@
 <?php 
     session_start();
 
-    if (!isset($_SESSION['username'])) {
+    if (!isset($_SESSION['stu_id'])) {
         header('location: auth/login.php');
-    }
-
-    if (isset($_GET['logout'])) {
-        session_destroy();
-        unset($_SESSION['username']);
-        header('location: login.php');
+    } else {
+        header('location: dash/index.php');
     }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/lib/css/bootstrap.min.css" rel=stylesheet>
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
