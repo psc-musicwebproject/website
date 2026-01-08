c6036e2aca02e48c95ae35a38de93ad352730e69
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-01-08 17:41:06 +0700

    feat: reverb support

--- resources/views/components/dash/layout.blade.php
+++ resources/views/components/dash/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $AppSetting::getSetting('name') ?? config('app.name', "PSC-MusicWebProject")}} - {{ $title ?? 'Dashboard' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -18,6 +19,7 @@
             <div class="app-content">
                 {{ $slot }}
             </div>
+
         </main>
     </div>
 </body>
