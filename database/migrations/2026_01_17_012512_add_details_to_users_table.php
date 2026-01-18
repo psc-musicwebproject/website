1cce36ab62723b94c9ad58049aebe9af1d40f479
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-01-18 10:25:25 +0700

    feat: addition user details

---
+++ database/migrations/2026_01_17_012512_add_details_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name_title')->nullable()->after('username');
+            $table->string('nickname')->nullable()->after('name_title');
+            $table->string('major')->nullable()->after('nickname');
+            $table->string('phone_number')->nullable()->after('major');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
