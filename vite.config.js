e49c1df66a8705ea039d94c2501fa24b2dc279df
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-01-27 09:14:03 +0000

    fix: codespace for 100th time

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         host: '0.0.0.0',
         hmr: {
             host: process.env.CODESPACE_NAME
-                ? `${process.env.CODESPACE_NAME}-5173.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`
+                ? `${process.env.CODESPACE_NAME}-5173.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN || 'app.github.dev'}`
                 : 'localhost',
             clientPort: process.env.CODESPACE_NAME ? 443 : 5173,
         },
