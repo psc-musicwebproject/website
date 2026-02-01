8670928fdbc265ba8d0466ec0d04623c5bddacd5
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-02-01 20:05:27 +0000

    feat: lang pack

---
+++ lang/th/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    "All rights reserved." => "สงวนลิขสิทธิ์",
+    "Regards," => "ด้วยความเคารพ",
+    "Hello!" => "สวัสดี!",
+    "Whoops!" => "ขออภัย!",
+    "If you're having trouble clicking the \":actionText\" button, copy and paste the URL below\ninto your web browser:" => "หากคุณมีปัญหาในการคลิกปุ่ม \":actionText\" ให้คัดลอกและวาง URL ด้านล่างนี้\nลงในเว็บเบราว์เซอร์ของคุณ:"
+];
