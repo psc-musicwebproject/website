4dc804752e48b87aad6b356fcd2eca377151e546
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-02-18 15:26:07 +0700

    qol: fix the statement for ClubApproval

--- app/Http/Controllers/ClubApprovalController.php
+++ app/Http/Controllers/ClubApprovalController.php
@@ -12,23 +12,23 @@ class ClubApprovalController extends Controller
     public function update(Request $request, $id)
     {
         $clubMember = \App\Models\ClubMember::where('member_id', $id)->firstOrFail();
-        $isThisAnUpdate = $clubMember->approval_status !== 'waiting';
+        $isInitialDecision = $clubMember->approval_status === 'waiting';
         $action = $request->input('action');
         $reason = $request->input('approve_reason');
         $adminId = \Illuminate\Support\Facades\Auth::id();
 
         if ($action === 'approve') {
             $clubMember->approve($adminId, $reason);
-            if (!$isThisAnUpdate) {
+            if ($isInitialDecision) {
                 $clubMember->user->notify(new ApprovedUserNotify($clubMember));
             }
             return redirect()->route('admin.club.approve')->with('success', 'อนุมัติสมาชิกเรียบร้อยแล้ว');
         } elseif ($action === 'reject') {
             $clubMember->reject($adminId, $reason);
-            if (!$isThisAnUpdate) {
+            if ($isInitialDecision) {
                 $clubMember->user->notify(new DeniedUserNotify($clubMember));
             }
-            return redirect()->route('admin.club.approve')->with('success', 'ไม่อนุมัติสมาชิกเรียบร้อยแล้ว');
+            return redirect()->route('admin.club.approve')->with('success', 'ปฎิเสธการสมาชิกเรียบร้อยแล้ว');
         }
 
         return back()->with('error', 'Invalid action');
