ff4a924164f81adef9c83a9508affa64d0c87b60
Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-02-18 15:05:10 +0700

    fix: the app use default link (line.me) instead of the desire link

--- app/Notifications/User/Booking/UserInvitedNotify.php
+++ app/Notifications/User/Booking/UserInvitedNotify.php
@@ -110,6 +110,8 @@ class UserInvitedNotify extends Notification
         $fTem['body']['contents'][2]['contents'][3]['contents'][1]['text'] =
             !empty($attendeeList) ? implode(', ', $attendeeList) : 'ไม่ระบุผู้เข้าร่วม';
 
+        $fTem['footer']['contents'][0]['action']['uri'] = route('booking.detail', ['booking_id' => $this->booking->booking_id]);
+
         $lineCon = new LineIntegrationController();
         return $lineCon->pushFlexMessage($notifiable->line_id, "คำเชิญเข้าห้อง" . $this->booking->room->room_name . " - " . $this->booking->booking_name, $fTem);
     }
