442b7757b590d2608a798ecfd83a484af8cf7faa
Cartoon Kritthapath Yaviraj (Toonshouin!) <krittapath6699@gmail.com>
2026-05-04 04:44:43 +0700

    rm: temporary databaseSeeder, left only dummy data for testing!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,26 +26,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::factory()->create([
-            'name' => 'Belno',
-            'student_id' => '032483',
-            'surname' => 'Light',
-            'username' => 'u6832483',
-            'type' => 'student',
-            'class' => 'ปวส.1/7 (ม.)',
-            'password' => '7151',
-        ]);
-
-        User::factory()->create([
-            'name' => 'Agnes',
-            'student_id' => '032408',
-            'surname' => 'Tachyon',
-            'username' => 'u6832408',
-            'type' => 'student',
-            'class' => 'ปวส.1/6',
-            'password' => '8153',
-        ]);
-
-        User::factory()->create([
             'name' => 'Tazuna',
             'student_id' => '992789',
             'surname' => 'Hayakawa',
@@ -63,16 +43,6 @@ class DatabaseSeeder extends Seeder
             'class' => null,
             'type' => 'admin',
             'password' => '7777',
-        ]);
-
-        User::factory()->create([
-            'name' => 'ประณต',
-            'student_id' => '996209',
-            'surname' => 'สว่างพิศาลกิจ',
-            'username' => '996209',
-            'class' => null,
-            'type' => 'admin',
-            'password' => 'Tong1234',
         ]);
 
     }
